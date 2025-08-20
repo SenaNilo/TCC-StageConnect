a841042aaa480f74e579e977a22e71c01b08b7cc
SenaNilo <danilo.sena005@gmail.com>
2025-08-19 21:36:26 -0300

    Inicializacao da navbar dentro do sistema

--- resources/views/auth/cadastro.blade.php
+++ resources/views/auth/cadastro.blade.php
@@ -13,6 +13,7 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
 	{{-- Css's --}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/init.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/util.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}">
 	

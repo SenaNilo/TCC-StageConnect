a585c3cc546095fb053c22bf201b1467806bb20c
SenaNilo <danilo.sena005@gmail.com>
2025-11-18 23:03:26 -0300

    fx. vite

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,28 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 
-                'resources/css/aluno.css',
-                'resources/js/app.js'],
+            input: [// Arquivos CSS principais que o Vite deve processar
+                'resources/css/app.css',
+                'resources/css/aluno.css',          
+                
+                // Se você tiver um CSS para conteúdos, inclua também
+                
+                'resources/css/alunos/aluno.css', 
+                'resources/css/alunos/configuracoes.css', 
+                'resources/css/alunos/conteudo-detalhe.css', 
+                'resources/css/alunos/conteudos.css', 
+                'resources/css/alunos/curriculo-resultado.css', 
+                'resources/css/alunos/curriculo.css', 
+                'resources/css/alunos/entrevistas.css', 
+
+                // Scripts JS principais
+                'resources/js/app.js',
+                'resources/js/bootstrap.js',
+                'resources/js/alunos/script-aluno.js',
+                'resources/js/alunos/modalSucesso.js',
+                'resources/js/alunos/modalSair.js',
+                
+            ],
             refresh: true,
         }),
         tailwindcss(),
